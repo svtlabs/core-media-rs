d5d9206168379a6a83e08f1df3cc6cb6e868025c
Per Johansson <per.johansson.1@svt.se>
2024-03-01 13:19:47 +0100

    feat: CMSampleBuffer and CMFormatDescription

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod cm_format_description_ref;
+pub mod cm_sample_buffer;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
